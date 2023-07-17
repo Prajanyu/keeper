7438801a967f74923ba8d6513f68fda77aaad743
Prajanyu <p98kuratkar@gmail.com>
2023-07-17 14:34:35 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const date = new Date();
+
+let year = date.getFullYear();
+
+function Footer() {
+    return (
+        <footer className="footer"> 
+            <p>Copyright {year}</p>
+        </footer>
+    );
+}
+
+export default Footer;
